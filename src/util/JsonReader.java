74ec30b2bc30de6bd8ddbaba874d0cbd219b4c05
Tvax <19556689+Tvax@users.noreply.github.com>
2017-12-19 13:40:08 +0100

    cleaning code

--- src/util/JsonReader.java
+++ src/util/JsonReader.java
@@ -7,7 +7,7 @@ import java.nio.charset.Charset;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class JsonReader {
+public abstract class JsonReader {
 
     private static String readAll(Reader rd) throws IOException {
         StringBuilder sb = new StringBuilder();
